b51a2415525dd43813f53c0d0fbe8c218e957731
Michael-Otieno <michael.otieno@student.moringaschool.com>
2022-05-15 15:45:08 +0300

    add navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,8 +1,28 @@
+import './Navbar.css'
+
+import hamburger from '../../images/icon-menu.svg'
+import cart from '../../images/icon-cart.svg'
+import avatar from '../../images/image-avatar.png'
+
 
 export default function Navbar(){
   return (
-    <div>
-      Navbar 
+    <div className="navbar">
+      <div className="hamburger--logo">
+        <div className="hamburger">
+          <img src={hamburger} alt="hamburger icon" />
+        </div>
+        <h1>sneakers</h1>
+      </div>
+
+      <div className="basket--profile">
+        <div>
+          <img className='cart' src={cart} alt="shopping cart"/>
+        </div>
+        <div>
+          <img src={avatar} alt="shopping cart" width="40px"/>
+        </div>
+      </div>
     </div>
   )
 }
