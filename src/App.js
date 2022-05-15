fb0dd1e505b44c0bc676b10ab48b56f17ed89bcb
Michael-Otieno <michael.otieno@student.moringaschool.com>
2022-05-16 01:16:37 +0300

    add cart page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
-
+// import Cart from './components/Cart/Cart.js'
 
 function App() {
+
+  
+
   return (
     <div className="App">
-      <Navbar />
+      <Navbar 
+      />
       MMMMMMMM
     </div>
   );
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,11 +1,61 @@
 import './Navbar.css'
+import {useState} from 'react'
 
 import hamburger from '../../images/icon-menu.svg'
 import cart from '../../images/icon-cart.svg'
 import avatar from '../../images/image-avatar.png'
 
 
-export default function Navbar(){
+const HoverDivHandler = ({handleMouseOver, handleMouseOut }) => {
+  return (
+    <div  className="basket--profile">
+      <div >
+        <img 
+        className='cart'
+        src={cart} 
+        alt="shopping cart"
+        onMouseOver={handleMouseOver}
+        onMouseOut={handleMouseOut}
+      />
+      </div>
+      <div >
+        <img 
+        className="avatar" 
+        src={avatar} 
+        alt="shopping cart" 
+        onMouseOver={handleMouseOver}
+        onMouseOut={handleMouseOut}
+      />
+      </div>
+    </div>
+  )
+}
+
+const HoverResultDiv = () => {
+  return (
+    <div className='hoverresultdiv'>
+      <div className='cart-title'>
+        <h2>Cart</h2>
+      </div>
+      <div className='cart-empty'>
+        <p>Your cart is empty</p>
+      </div>
+    </div>
+  )
+}
+
+
+export default function Navbar(props){
+
+  const [isHovering, setIsHovering] = useState(false);
+  const handleMouseOver = () => {
+    setIsHovering(true);
+  }
+
+  const handleMouseOut = () => {
+    setIsHovering(false);
+  }
+  
   return (
     <div className="navbar">
       <div className="hamburger--logo">
@@ -14,15 +64,11 @@ export default function Navbar(){
         </div>
         <h1>sneakers</h1>
       </div>
-
-      <div className="basket--profile">
-        <div>
-          <img className='cart' src={cart} alt="shopping cart"/>
-        </div>
-        <div>
-          <img src={avatar} alt="shopping cart" width="40px"/>
-        </div>
-      </div>
+      <HoverDivHandler 
+      handleMouseOver={handleMouseOver}
+      handleMouseOut={handleMouseOut}
+      />
+      {isHovering && <HoverResultDiv />} 
     </div>
   )
 }
