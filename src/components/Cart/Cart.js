94084b173d8d0da9fb6dd4198f023e227983300f
Michael-Otieno <michael.otieno@student.moringaschool.com>
2022-05-17 16:34:09 +0300

    add cart page

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,6 +1,34 @@
+import { useState } from 'react'
+
+import './Cart.css'
+import cart from '../../images/icon-cart.svg'
 
 export default function Cart(){
+  const [showCart, setShowCart] = useState(true);
+
+  function toggleCart(){
+    setShowCart(prevMode => !prevMode)
+  }
+ 
   return (
-    <div>Cart</div>
+    <div className='cart--toggle'>
+      <div className='cart--total'>
+        <img 
+          className='cart'
+          src={cart} 
+          alt="shopping cart"
+          onClick={toggleCart}
+
+        />
+        <span className='total'>3</span>
+      </div>
+      <div className={showCart ? "toggle": "no-toggle"}>
+        <h2 className='cart--title'>Cart</h2>
+        <hr />
+        <div className='cart--content'>
+          <p className='cart--empty'>Your cart is empty</p>
+        </div>
+      </div>
+    </div>
   )
 }
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,15 +1,15 @@
 import './Navbar.css'
+import Cart from '../Cart/Cart'
 import {useState} from 'react'
 
 import hamburger from '../../images/icon-menu.svg'
-import cart from '../../images/icon-cart.svg'
 import avatar from '../../images/image-avatar.png'
 import logo from '../../images/logo.svg'
 
 
-console.log(logo)
 export default function Navbar(props){
 
+  
   
   return (
     <div className="navbar">
@@ -23,24 +23,16 @@ export default function Navbar(props){
       </div>
 
       <div  className="basket--profile">
-        <div className='cart--total'>
-          <img 
-        className='cart'
-        src={cart} 
-        alt="shopping cart"
-     
-      />
-      <span className='total'>3</span>
-      </div>
+        <Cart />
       <div >
         <img 
         className="avatar" 
         src={avatar} 
         alt="shopping cart" 
-      />
+        />
       </div>
     </div>
-      
+    
     </div>
   )
 }
